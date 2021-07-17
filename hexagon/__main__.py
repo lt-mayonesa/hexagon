c72de855b7f8a0e236a9ea100d0e2662f8d90895
Joaco Campero <joaquin@redb.ee>
2021-07-16 23:15:29 -0300

    fix: simplify always true cli.command

--- hexagon/__main__.py
+++ hexagon/__main__.py
@@ -50,27 +50,21 @@ def main():
 
         log.finish()
 
-        # TODO: cli.command is always true
-        #
-        # with the addition of pydantic cli.command is always set.
-        # we should find another way of knowing if executing hexagon or a project cli
-        # the same goes for the if at line 25
-        if cli.command:
-            if tracer.has_traced():
-                log.extra(
-                    "[cyan dim]Para repetir este comando:[/cyan dim]",
-                    f"[cyan]     {cli.command} {tracer.command()}[/cyan]",
-                )
-                command_as_aliases = tracer.command_as_aliases(tools, envs)
-                if command_as_aliases:
-                    log.extra(
-                        "[cyan dim]  o:[/cyan dim]",
-                        f"[cyan]     {cli.command} {command_as_aliases}[/cyan]",
-                    )
-            store_user_data(
-                HexagonStorageKeys.last_command.value,
-                f"{cli.command} {tracer.command()}",
+        if tracer.has_traced():
+            log.extra(
+                "[cyan dim]Para repetir este comando:[/cyan dim]",
+                f"[cyan]     {cli.command} {tracer.command()}[/cyan]",
             )
+            command_as_aliases = tracer.command_as_aliases(tools, envs)
+            if command_as_aliases:
+                log.extra(
+                    "[cyan dim]  o:[/cyan dim]",
+                    f"[cyan]     {cli.command} {command_as_aliases}[/cyan]",
+                )
+        store_user_data(
+            HexagonStorageKeys.last_command.value,
+            f"{cli.command} {tracer.command()}",
+        )
     except KeyboardInterrupt:
         sys.exit(1)
 
