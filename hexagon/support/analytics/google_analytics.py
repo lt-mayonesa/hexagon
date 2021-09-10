18a621ed1b6998644ff5bcdb59218393fc7e726f
Joaco Campero <joaquin@redb.ee>
2021-09-09 23:59:09 -0300

    fix(analytics): register user_id and client_id

--- hexagon/support/analytics/google_analytics.py
+++ hexagon/support/analytics/google_analytics.py
@@ -3,21 +3,27 @@ import uuid
 import pkg_resources
 import requests
 
-from hexagon.support.storage import load_user_data, store_user_data
+from hexagon.support.storage import load_user_data, store_user_data, HEXAGON_STORAGE_APP
 
 
 def event(name: str, **kwargs):
     version = pkg_resources.require("hexagon")[0].version
 
-    cid = load_user_data("user_id")
+    cid = load_user_data("client_id", app=HEXAGON_STORAGE_APP)
     if not cid:
         cid = str(uuid.uuid4())
-        store_user_data("user_id", cid)
+        store_user_data("user_id", cid, app=HEXAGON_STORAGE_APP)
+
+    uid = load_user_data("user_id")
+    if not uid:
+        uid = str(uuid.uuid4())
+        store_user_data("user_id", uid)
 
     mid = "G-Y28H5KHQEZ"
 
     params = {
         "client_id": cid,
+        "user_id": uid,
         "events": [{"name": name, "params": {**kwargs}}],
         "user_properties": {"hexagon_version": {"value": version}},
     }
