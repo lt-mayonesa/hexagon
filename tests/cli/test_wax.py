c7eb049240204f0e6f076197ee215a1418d41990
Joaco Campero <juacocampero@gmail.com>
2023-03-12 21:32:15 -0300

    feat(analytics): remove support for tracking

--- tests/cli/test_wax.py
+++ tests/cli/test_wax.py
@@ -3,9 +3,7 @@ from InquirerPy import inquirer
 
 from hexagon.domain.tool import Tool
 from hexagon.domain.env import Env
-from hexagon.support.analytics import UserEvent
 from hexagon.support.wax import search_by_name_or_alias, select_tool, select_env
-from hexagon.support import analytics
 
 
 def args_mock(ret):
@@ -34,16 +32,6 @@ def env_mock():
     return args_mock("dev")
 
 
-@pytest.fixture
-def analytics_mock():
-    class TestArgs(object):
-        def __call__(self, e: UserEvent, **kwargs):
-            self.args = {"e": e, **kwargs}
-            return
-
-    return TestArgs()
-
-
 tools = [
     Tool(name="docker", alias="d", action="docker_run"),
     Tool(name="bastion", alias="b", action="bastion"),
@@ -69,17 +57,14 @@ def test_search_tools_dict_by_key_or_alias(search, expected):
     assert search_by_name_or_alias(tools, search) == expected
 
 
-def test_tool_is_selected_from_cmd(monkeypatch, analytics_mock):
-    monkeypatch.setattr(analytics, "user_event", analytics_mock)
-
+def test_tool_is_selected_from_cmd(monkeypatch):
     assert select_tool(tools, "docker") == Tool(
         name="docker", alias="d", action="docker_run"
     )
 
 
-def test_tool_is_selected_by_prompt(monkeypatch, tool_mock, analytics_mock):
+def test_tool_is_selected_by_prompt(monkeypatch, tool_mock):
     monkeypatch.setattr(inquirer, "fuzzy", tool_mock)
-    monkeypatch.setattr(analytics, "user_event", analytics_mock)
 
     assert select_tool(tools) == Tool(name="docker", alias="d", action="docker_run")
     assert tool_mock.args[0] == "action.support.wax.select_tool"
@@ -99,17 +84,14 @@ def test_tool_has_env_property_with_wildcard():
     assert select_env(envs, {"*": "sarasa"}) == (None, "sarasa")
 
 
-def test_env_is_selected_from_cmd(monkeypatch, analytics_mock):
-    monkeypatch.setattr(analytics, "user_event", analytics_mock)
-
+def test_env_is_selected_from_cmd(monkeypatch):
     tool_envs = {"dev": "env_1", "qa": "env_2"}
     assert select_env(envs, tool_envs, "qa") == (envs[1], "env_2")
 
 
-def test_env_is_selected_by_prompt(monkeypatch, env_mock, analytics_mock):
+def test_env_is_selected_by_prompt(monkeypatch, env_mock):
     tool_envs = {"dev": "env_1", "qa": "env_2"}
     monkeypatch.setattr(inquirer, "fuzzy", env_mock)
-    monkeypatch.setattr(analytics, "user_event", analytics_mock)
 
     assert select_env(envs, tool_envs) == (envs[0], "env_1")
     assert env_mock.args[0] == "action.support.wax.select_environment"
