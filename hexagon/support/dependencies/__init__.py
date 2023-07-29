c0a0f2ed15fa186a4dd05c9d196e4467497f7394
Joaquin Campero <joaquin.campero@teampicnic.com>
2023-07-29 02:25:41 +0200

    fix(dependencies): show progress by individual install

--- hexagon/support/dependencies/__init__.py
+++ hexagon/support/dependencies/__init__.py
@@ -3,7 +3,6 @@ import os
 from hexagon.domain.singletons import options
 from hexagon.support.dependencies.node import scan_and_install_node_dependencies
 from hexagon.support.dependencies.python import scan_and_install_python_dependencies
-from hexagon.support.printer import log
 
 HEXAGON_DEPENDENCY_UPDATER_MOCK_ENABLED_ENVIRONMENT_VARIABLE = (
     "HEXAGON_DEPENDENCY_UPDATER_MOCK_ENABLED"
@@ -23,16 +22,6 @@ def scan_and_install_dependencies(path: str = None):
         == "1"
     )
 
-    with log.status(
-        _("msg.support.dependencies.installing_dependencies").format(
-            runtime="python", path=path
-        )
-    ):
-        scan_and_install_python_dependencies(path, mocked)
+    scan_and_install_python_dependencies(path, mocked)
 
-    with log.status(
-        _("msg.support.dependencies.installing_dependencies").format(
-            runtime="node", path=path
-        )
-    ):
-        scan_and_install_node_dependencies(path, mocked)
+    scan_and_install_node_dependencies(path, mocked)
