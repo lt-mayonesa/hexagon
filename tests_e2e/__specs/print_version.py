215a7d8a3d8e1e1e82c372cabd31981ad5eb47e4
semantic-release <semantic-release>
2025-05-06 12:48:32 +0000

    chore(release): 0.62.1
    Automatically generated by python-semantic-release

--- tests_e2e/__specs/print_version.py
+++ tests_e2e/__specs/print_version.py
@@ -3,7 +3,7 @@ import pytest
 from tests_e2e.__specs.utils.hexagon_spec import as_a_user
 
 # this updates automatically https://python-semantic-release.readthedocs.io/en/latest/index.html
-__version__ = "0.62.0"
+__version__ = "0.62.1"
 
 
 @pytest.mark.parametrize("command", ["--version", "-v"])
