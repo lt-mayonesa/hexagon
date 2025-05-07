27a434be5ff89d0b37c1e1adf79755732e3fd8b4
Joaquin Campero <joaquin.campero@teampicnic.com>
2025-05-08 00:11:56 +0200

    chore(e2e): fix create new tool

--- tests_e2e/__specs/create_new_tool.py
+++ tests_e2e/__specs/create_new_tool.py
@@ -1,9 +1,9 @@
 import os
 import shutil
+from pathlib import Path
 
 from tests_e2e.__specs.utils.config import read_hexagon_config
 from tests_e2e.__specs.utils.hexagon_spec import HexagonSpec, as_a_user
-from tests_e2e.__specs.utils.path import e2e_test_folder_path
 
 LONG_NAME = "Custom Action Test"
 DESCRIPTION = "Hexagon Custom Action Test Description"
@@ -147,14 +147,10 @@ def test_create_new_python_module_tool():
     assert created_tool["type"] == "shell"
     assert created_tool["alias"] == "anac"
 
-    assert os.path.isfile(
-        os.path.join(e2e_test_folder_path(spec.test_dir), "a-new-action/__init__.py")
-    )
-    assert os.path.isfile(
-        os.path.join(e2e_test_folder_path(spec.test_dir), "a-new-action/README.md")
-    )
+    assert os.path.isfile(os.path.join(spec.test_dir, "a-new-action/__init__.py"))
+    assert os.path.isfile(os.path.join(spec.test_dir, "a-new-action/README.md"))
 
-    shutil.rmtree(os.path.join(e2e_test_folder_path(spec.test_dir), "a-new-action"))
+    shutil.rmtree(os.path.join(spec.test_dir, "a-new-action"))
 
 
 def test_create_tool_creates_custom_tools_dir():
@@ -164,6 +160,11 @@ def test_create_tool_creates_custom_tools_dir():
 
     spec = (
         as_a_user(__file__)
+        .executing_first(
+            lambda spec: Path(spec.test_dir)
+            .joinpath(custom_tools_dir_name)
+            .mkdir(exist_ok=True, parents=True)
+        )
         .given_a_cli_yaml(app_file)
         .run_hexagon()
         .with_shared_behavior(_shared_assertions)
@@ -184,7 +185,7 @@ def test_create_tool_creates_custom_tools_dir():
                 "Where would you like it to be? (can be absolute path or relative to YAML",
                 "",
             ],
-            True,
+            discard_until_first_match=True,
         )
         .input(f"/{custom_tools_dir_name}")
         .exit()
@@ -195,16 +196,8 @@ def test_create_tool_creates_custom_tools_dir():
     assert config_file["cli"]["custom_tools_dir"] == f"./{custom_tools_dir_name}"
 
     assert os.path.isfile(
-        os.path.join(
-            e2e_test_folder_path(spec.test_dir),
-            custom_tools_dir_name,
-            "a-new-action/__init__.py",
-        )
+        os.path.join(spec.test_dir, custom_tools_dir_name, "a-new-action/__init__.py")
     )
     assert os.path.isfile(
-        os.path.join(
-            e2e_test_folder_path(spec.test_dir),
-            custom_tools_dir_name,
-            "a-new-action/README.md",
-        )
+        os.path.join(spec.test_dir, custom_tools_dir_name, "a-new-action/README.md")
     )
--- tests_e2e/__specs/utils/config.py
+++ tests_e2e/__specs/utils/config.py
@@ -2,8 +2,6 @@ import os
 
 from ruamel.yaml import YAML
 
-from tests_e2e.__specs.utils.path import e2e_test_folder_path
-
 
 def write_hexagon_config(test_file: str, config):
     path = os.path.join(test_file, "app.yml")
@@ -13,5 +11,5 @@ def write_hexagon_config(test_file: str, config):
 
 
 def read_hexagon_config(test_file: str):
-    with open(os.path.join(e2e_test_folder_path(test_file), "app.yml"), "r") as file:
+    with open(os.path.join(test_file, "app.yml"), "r") as file:
         return YAML().load(file)
